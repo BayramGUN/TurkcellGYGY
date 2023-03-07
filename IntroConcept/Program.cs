158c7d7360cf1012da9f628985ddc1942a4e1b07
BayramGUN <bayram.gun635@gmail.com>
2023-03-07 20:33:36 +0300

    first class

---
+++ IntroConcept/Program.cs
@@ -0,0 +1,16 @@
+﻿
+byte x = 2;
+short y = 521;
+
+try
+{
+    checked
+    {
+        byte result = (byte) (y / x);
+        Console.Write(result);
+    }
+}
+catch (OverflowException)
+{
+    Console.WriteLine("toplamda byte sınırlarını aştınız");
+}
