0a7287b0215b8951e59b435fd7009bbe5f2b1864
BayramGUN <bayram.gun635@gmail.com>
2023-03-07 20:34:32 +0300

    Conditional process of tries and cathes

---
+++ TryAndCatch/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+try
+{
+
+    Console.WriteLine("Sayi gir ula");
+    var sayiBir = Convert.ToInt32(Console.ReadLine());
+    Console.Write(" gibi bir hata oluştu");
+
+    Console.WriteLine("Bir sayi daha gir bea");
+    int sayiIki = Convert.ToInt32(Console.ReadLine());
+    
+    int bolum = sayiBir / sayiIki;
+
+    Console.WriteLine($"Bolme isleminin sonucu: {bolum}");
+}
+catch (System.FormatException)
+{
+    
+    Console.WriteLine("ergeen ergen sayi gir ergen");
+}
+catch (DivideByZeroException) 
+{
+    Console.WriteLine("matematik ogrenmeniz rica olunur");
+}
+catch(Exception ex) 
+{
+    Console.WriteLine($"{ex.Message} gibi bir hata oluştu");
+}
+finally
+{
+    Console.Write("illa calisacak");
+}
