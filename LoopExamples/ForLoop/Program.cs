2a7b4b7c5344ba51cd917a185f674759e3566df5
BayramGUN <bayram.gun635@gmail.com>
2023-03-09 21:37:14 +0300

    For and While loops examples

---
+++ LoopExamples/ForLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿/* int[] numbers = { 13, 46, 0, 1, 18, -9};
+
+int length = numbers.Length;
+
+
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+ */
+
+ string[] countries = { "Türkiye", "Germany", "Netherland", "Sweden", "Cyprus" };
+
+ Console.WriteLine("Please enter a country:");
+
+ string? country = Convert.ToString(Console.ReadLine()).ToLower();
+
+ bool isFind = false;
+
+ for (int i = 0; i < countries.Length; i++)
+ {
+    if(countries[i].ToLower() == country) 
+    {
+        isFind = true;
+        break;
+    }
+ }
+
+ if (isFind) Console.WriteLine("Vordır");
+ else Console.WriteLine("Yogdir");
---
+++ LoopExamples/WhileLoop/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* Console.WriteLine("Please enter a suggest:");
+
+bool isWin = false;
+
+Random randomNumberGenerator = new Random();
+
+int puzzle = randomNumberGenerator.Next(0, 100);
+while(!isWin) 
+{
+    int suggest = Convert.ToInt16(Console.ReadLine());
+
+    if(suggest > puzzle) 
+        Console.WriteLine("Down!");
+
+    else if(suggest < puzzle) 
+        Console.WriteLine("Up!");
+
+    else
+    {
+        isWin = true;
+        Console.WriteLine("Congrats!");
+    } 
+}
+ */
+
+
+int[] numbers = { 13, 46, 0, 1, 18, -9};
+
+int index = 0;
+while(index < numbers.Length) {
+    Console.WriteLine(numbers[index++]);
+}
