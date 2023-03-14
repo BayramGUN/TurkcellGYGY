a7a83b148c6b303a93a2dcecf0373ad83634ad1d
BayramGUN <bayram.gun635@gmail.com>
2023-03-14 18:35:54 +0300

    SecondWeek first homework added

---
+++ HomeWorks/SecondWeek/PasswordStrength/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter your password");
+
+string? password = Convert.ToString(Console.ReadLine());
+
+
+bool hasDigit = false;
+bool hasLetter = false;
+bool hasSymbol = false;
+bool hasLetterOrDigit = false;
+if(password.Length >= 6)
+{
+    foreach(char ch in password) 
+    {
+        if(char.IsDigit(ch)) hasDigit = true;
+        else if(char.IsLetter(ch)) hasLetter = true;
+        if(!char.IsLetterOrDigit(ch)) 
+        {
+            hasSymbol = true;
+        }
+        else 
+            hasLetterOrDigit = true;
+    }
+}
+else 
+    Console.WriteLine("Please enter new password includes min. 6 character");
+
+if(hasLetter && hasDigit && hasSymbol)
+    Console.WriteLine("Password is strength!");
+else if((hasLetter && hasDigit) || (hasSymbol && hasLetterOrDigit))
+    Console.WriteLine("Password is mid!");
+else if(hasLetter || hasDigit || hasSymbol)
+    Console.WriteLine("Password is weak!");
+
