b36dd859f06f3576dadf4473fd3c81364cf5b710
BayramGUN <bayram.gun635@gmail.com>
2023-03-21 18:58:17 +0300

    NullConcepts

---
+++ OOP/NullConcepts/GenericTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+/* Console.WriteLine("Hello, World!");
+
+ArrayList words = new ArrayList();
+
+words.Add(15);
+
+Console.WriteLine((int)words[0]);  */
+
+GenericPoint<decimal> dec = new GenericPoint<T>();
+
+T GetTypeOfGeneric<T>(T type)
+{
+    return type.GetType().Name;
+}
+public class GenericPoint<T> 
+    where T : struct
+{
+    public T X { get; set; }
+    public T Y { get; set; }
+    public T Z { get; set; }
+
+    public void Reset()
+    {
+        X = default(T);
+        Y = default(T);
+        Z = default(T);
+    }
+}
+
---
+++ OOP/NullConcepts/NullOperations/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// boxing: put a value into a box.
+
+// everything comes from 'object'
+object o = "an object";
+o = 9;
+o = false;
+o = new Random();
+object b = "an";
+o = "an";
+bool yaz = o.Equals(b) ? true:false;
+/* if(o == b) 
+    Console.WriteLine(yaz);
+ */
+
+// '==' compares to referances
+// .Equals compares to values
+
+
+Address addressOne = new Address() { City = "hatay" };
+Address addressTwo = new Address() { City = "hatay" };
+Address addressThree = addressOne; 
+
+Console.WriteLine(addressOne.ToString());
+/* 
+Console.WriteLine(addressOne.Equals(addressTwo));
+Console.WriteLine(addressOne == addressTwo);
+Console.WriteLine(addressOne == addressThree);
+Console.WriteLine(addressOne.Equals(addressThree)); */
+public class Address
+{
+    public string City { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        Address? that = (Address?) obj;
+
+        return City.Equals(that?.City);
+    }
+    public override string ToString()
+    {
+        return $"This adress at the {City}";
+    }
+} 
+/* 
+string tryingOne = "TURKCELL";
+string tryingTwo = "TURKCELL";
+string tryingThree = "TURKCELLx".Substring(0, 8);
+object tryingFour = tryingThree;
+
+Console.WriteLine($"deney1 == deney2: {tryingOne == tryingTwo}");
+Console.WriteLine($"deney1 == deney3: {tryingOne == tryingThree}");
+Console.WriteLine($"deney1.Equals(deney2): {tryingOne.Equals(tryingTwo)}");
+Console.WriteLine($"deney1.Equals(deney3): {tryingOne.Equals(tryingThree)}");
+Console.WriteLine($"deney1.Equals(deney4): {tryingOne.Equals(tryingFour)}");
+Console.WriteLine($"deney1 {tryingOne.GetHashCode()} == deney4 {tryingFour.GetHashCode()}: {tryingOne == tryingFour}"); */
