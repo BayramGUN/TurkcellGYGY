7a43a0dd012c323992b42c33608d0370b3ef6b34
BayramGUN <bayram.gun635@gmail.com>
2023-06-10 19:51:47 +0300

    Some MVC Operations added and repositories ended

--- Homeworks/Assigments/BookmateApp/src/Presentation/WebUI/BookmateApp.Mvc/Controllers/AuthorController.cs
+++ Homeworks/Assigments/BookmateApp/src/Presentation/WebUI/BookmateApp.Mvc/Controllers/AuthorController.cs
@@ -27,12 +27,28 @@ namespace BookmateApp.Mvc.Controllers
             var authorForUpdate = await _authorService.GetAuthorForUpdateAsync(id);
             return View(authorForUpdate);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Guid id, UpdateAuthorRequest updateAuthorRequest)
+        {
+            if(await _authorService.IsAuthorExist(id))
+            {
+                if(ModelState.IsValid)
+                {
+                    await _authorService.UpdateAuthorAsync(updateAuthorRequest);
+                    return RedirectToAction(nameof(Index));
+                }
+                return View();
+            }
+            return NotFound();
+        }
+
+
         public IActionResult CreateAuthor()
         {
             return View();
         }
         
-
         [HttpPost]
         public async Task<IActionResult> CreateAuthor(CreateAuthorRequest createAuthorRequest)
         {
